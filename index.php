feb9021c607e799cf2f52512a52e287504be24ea
Abid004 <92315375+Abid004@users.noreply.github.com>
2022-09-08 10:27:44 +0200

    powsh

---
+++ index.php
@@ -0,0 +1,24 @@
+OCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deep(red)Notes</title>
+</head>
+<body bgcolor="#ff1493"> // here you can put bagground color for you App
+    <form method="post"> 
+        <input type="text" name="note">
+        <butto type="submit">Gem</button>
+    <form> 
+        
+    <ul>
+        <li>My one note </li>
+    <ul>
+
+</body>
+</html>
+<?php
+$HTTP_RAW_POST_DATA for "note"
+
+?>
